78b3dc8c9f5b124544d486f789aae180d1efe9c1
James Baxley <james@whitefloordesign.com>
2015-09-29 23:34:17 -0400

    making good progress on syntax tree

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-module.exports = require(__dirname + '/lib/hcl');
+"use strict";
+
+const Path = require("path"),
+      Fs = require("fs");
+
+
+class HCL {
+
+  constructor() {
+
+  }
+
+}
+
+// converts an object into an HCL string
+HCL.prototype.stringify = require("./lib/stringify");
+
+// parses an HCL string into an object
+HCL.prototype.parse = require("./lib/parse");
+
+// parses an HCL file into an object
+HCL.prototype.load = require("./lib/load");
+
+// requires an HCL file and returns the result object
+HCL.prototype.require = require("./lib/require");
+
+
+module.exports = new HCL()
---
+++ test.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const HCL = require("./index");
+
+
+const object = HCL.load("./Norma");
+
+console.log(object);
