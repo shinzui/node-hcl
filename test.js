5bbdabb57a0c2a77bbe1d3588fac260e1d342504
James Baxley <james@whitefloordesign.com>
2015-10-02 12:39:55 -0400

    started work on stringifying methods

--- test.js
+++ test.js
@@ -4,8 +4,28 @@ const HCL = require("./index")(".tf"),
       PrettyPrint = require("prettyjson");
 
 
-const object = require("./test-config");
+// const object = HCL.load("./AppFile");
+//
+// console.log("\n--- Lets see how we did ---\n")
+// console.log(PrettyPrint.render(object));
 
-console.log("\n--- Lets see how we did ---\n")
-console.log(PrettyPrint.render(object));
+const object = {
+  "application": {
+    "awesomeString": "string",
+    "awesomeNumber": 100,
+    "awsomeBoolean": true,
+    "array": [
+      "string",
+      true,
+      10
+    ],
+    "object": {
+      "key": "value"
+    }
+  }
+}
+
+
+
+console.log(HCL.stringify(object));
 // console.log(object)
