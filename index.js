3089b17b3c2322aa9150fe078ff1981a9bda80a8
James Baxley <james@whitefloordesign.com>
2015-10-02 01:06:18 -0400

    add ability to bind requiring of file types

--- index.js
+++ index.js
@@ -6,8 +6,57 @@ const Path = require("path"),
 
 class HCL {
 
-  constructor() {
+  constructor(fileTypes) {
 
+    this.fileTypes = false
+
+    if (Array.isArray(fileTypes)) {
+      this.fileTypes = fileTypes;
+    }
+
+    if (typeof(fileTypes) === "string") {
+      this.fileTypes = [fileTypes];
+    }
+
+    if (this.fileTypes) {
+      this.bindRequire();
+    }
+
+
+  }
+
+  // requires an HCL file and returns the result object
+  bindRequire() {
+    const Parser = this.parse;
+
+    function addRequire(file) {
+      if (require.extensions[file]) {
+        if (!require.extensions[file].__hcljs) {
+          console.warning(
+            "WARNING: Some other require implementation " +
+            "is already present for " + file + " type" +
+            ",aborting."
+          );
+        }
+        return;
+      }
+
+      require.extensions[file] = function (module, filename) {
+        const fs = require('fs'),
+            hcl = Parser(fs.readFileSync(filename, 'utf8'));
+
+        module.exports = hcl;
+      };
+
+      if (!require.extensions['.hcl']) {
+        require.extensions['.hcl'] = require.extensions[file];
+      }
+      require.extensions[file].__hcljs = true;
+    }
+
+    for (let fileType of this.fileTypes) {
+      addRequire(fileType);
+    }
   }
 
 }
@@ -21,8 +70,7 @@ HCL.prototype.parse = require("./lib/hcl").parse;
 // parses an HCL file into an object
 HCL.prototype.load = require("./lib/load");
 
-// requires an HCL file and returns the result object
-HCL.prototype.require = require("./lib/require");
 
-
-module.exports = new HCL()
+module.exports = function(opts) {
+  return new HCL(opts);
+}
--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 "use strict";
 
-const HCL = require("./index"),
+const HCL = require("./index")(".tf"),
       PrettyPrint = require("prettyjson");
 
 
-const object = HCL.load("./Norma");
+const object = require("./test-config");
 
 console.log("\n--- Lets see how we did ---\n")
 console.log(PrettyPrint.render(object));
